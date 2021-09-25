64884abf4f781487be3f33fb9c88f22487392568
Amr Abdelmagid <amr.abdelmegid@tuftstudios.com>
2021-09-25 08:07:07 +0200

    Remove app lists glow

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_bloc_dio_practice/constants/colors.dart';
+import 'package:flutter_bloc_dio_practice/presentation/my_scroll_behavior.dart';
 import 'app_router.dart';
 import 'business_logic/bloc_observer_helper/bloc_observer_helper.dart';
 
@@ -25,11 +26,17 @@ class AnimeApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        builder: (context, child) => ScrollConfiguration(
+              behavior: MyBehavior(),
+              child: child ?? Container(),
+            ),
         debugShowCheckedModeBanner: false,
         onGenerateRoute: appRouter.generateRoute,
         theme: ThemeData(
-            primaryColor: AppColors.myYellow,
-            colorScheme:
-                ColorScheme.fromSwatch(accentColor: AppColors.myGrey)));
+          primaryColor: AppColors.myYellow,
+          colorScheme: ColorScheme.fromSwatch(
+            accentColor: AppColors.myGrey,
+          ),
+        ));
   }
 }
